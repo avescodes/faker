319cf7c7e978f80f3880e5041d51bb189cb961b7
Alan Dipert <alan@dipert.org>
2012-06-30 22:58:52 -0400

    :exclude 'name' in faker ns

--- src/faker.clj
+++ src/faker.clj
@@ -1,5 +1,6 @@
 (ns faker
-  (:require [faker.data :as d]))
+  (:require [faker.data :as d])
+  (:refer-clojure :exclude (name)))
 
 (defn first-name [] (rand-nth d/first-names))
 (defn last-name [] (rand-nth d/last-names))
