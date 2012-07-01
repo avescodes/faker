d8de048ba3be4c6445735e7486f01da015a65dfa
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-06-30 23:24:24 -0400

    Move names into resources map

--- src/faker.clj
+++ src/faker.clj
@@ -1,6 +1,6 @@
-(ns faker
-  (:require [faker.data :as d]))
+(ns faker)
 
-(defn first-name [] (rand-nth d/first-names))
-(defn last-name [] (rand-nth d/last-names))
+(def ^:private data (read-string (slurp (.getFile (clojure.java.io/resource "data.clj")))))
+(defn first-name [] (rand-nth ((data :names) :first)))
+(defn last-name [] (rand-nth ((data :names) :last)))
 (defn name [] (str (first-name) " " (last-name)))
